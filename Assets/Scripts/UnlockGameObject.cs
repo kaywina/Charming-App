365e6518c5ce87c6811eff6706fcd7902855ad48
Kevin Afanasiff <kafanasiff@gmail.com>
2018-10-25 20:55:19 -0700

    prototype button for unlocking charm with currency; reusable UnlockGameObject script

---
+++ Assets/Scripts/UnlockGameObject.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockGameObject : MonoBehaviour {
+
+    public GameObject go;
+    public int cost;
+    public bool lockOnPlay = false;
+    
+	// Use this for initialization
+	void Start () {
+
+        if (lockOnPlay)
+        {
+            PlayerPrefs.SetString(go.name, "locked");
+        }
+
+        if (PlayerPrefs.GetString(go.name) == "unlocked")
+        {
+            go.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            go.SetActive(false);
+        }
+	}
+
+    public void Unlock()
+    {
+        if (CurrencyManager.WithdrawAmount(cost))
+        {
+            PlayerPrefs.SetString(go.name, "unlocked");
+            go.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
