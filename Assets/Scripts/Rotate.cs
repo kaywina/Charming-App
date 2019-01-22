eee31d760e248fafe36ea88ec5af718897838295
kafanasiff <kafanasiff@blueprintreality.com>
2019-01-22 12:24:02 -0800

    adding simple rotate script to stand icons

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+
+    public float speed = 1.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        transform.Rotate(Vector3.up * step);
+    }
+}
