847a5506f96a4c8e0a75abed7e8ba7d837917b4c
xacalite <anachronicdesigns@gmail.com>
2019-01-24 11:32:09 -0800

    adding yes/now confirmation panel after tapping any unlock button

---
+++ Assets/Scripts/UnlockButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockButton : MonoBehaviour {
+
+    public GameObject go;
+    public int cost;
+    private bool lockOnPlay = false;
+    public Text priceText;
+    public UnlockPanel unlockPanel;
+
+    void Start () {
+
+        priceText.text = cost.ToString();
+
+        if (lockOnPlay)
+        {
+            PlayerPrefs.SetString(go.name, "locked");
+            Debug.LogWarning("Warning! lockOnPlay is true");
+        }
+
+        if (PlayerPrefs.GetString(go.name) == "unlocked")
+        {
+            go.SetActive(true);
+            gameObject.SetActive(false);
+        }
+        else
+        {
+            go.SetActive(false);
+        }
+	}
+
+    public void GoToConfirmation()
+    {
+        unlockPanel.SetObjectToUnlock(go, gameObject); // pass in the object to unlock, and the current button object (so can disable it if unlock is confirmed)
+        unlockPanel.SetUnlockCost(cost);
+        unlockPanel.ShowPanel();
+    }
+}
---
+++ Assets/Scripts/UnlockPanel.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnlockPanel : MonoBehaviour
+{ 
+    public GameObject notEnoughText;
+    public Button yesButton;
+    public Text costText;
+    public int cost;
+    public GameObject toUnlock;
+    public GameObject unlockButton;
+
+    private void OnEnable()
+    {
+        if (CurrencyManager.WithdrawAmount(cost))
+        {
+            yesButton.interactable = true;
+            notEnoughText.SetActive(false);
+        }
+        else
+        {
+            yesButton.interactable = false;
+            notEnoughText.SetActive(true);
+        }
+    }
+
+    private void OnDisable()
+    {
+        cost = 0;
+        toUnlock = null;
+    }
+
+    public void ShowPanel()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void HidePanel()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void SetUnlockCost(int toSet)
+    {
+        cost = toSet;
+        costText.text = cost.ToString();
+    }
+
+    public void SetObjectToUnlock(GameObject toSet, GameObject button)
+    {
+        toUnlock = toSet;
+        unlockButton = button;
+    }
+
+    public void UnlockObject()
+    {
+        PlayerPrefs.SetString(toUnlock.name, "unlocked");
+        toUnlock.SetActive(true);
+        unlockButton.SetActive(false);
+        HidePanel();
+    }
+}
