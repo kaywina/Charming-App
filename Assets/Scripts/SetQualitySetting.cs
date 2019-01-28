456880c9bfd6178412a89239de693be60fec1d06
xacalite <anachronicdesigns@gmail.com>
2019-01-27 20:42:36 -0800

    replacing display of raw images by camera rig and raw image with world space objects (as was unable to remove jaggies from raw image otherwise); added a debug slider for quality settings; tweaked quality settings so that 2x, 4x, and 8x AA are available at three highest positions

---
+++ Assets/Scripts/SetQualitySetting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class SetQualitySetting : MonoBehaviour
+{
+    public Slider slider;
+
+    private void Start()
+    {
+        QualitySettings.SetQualityLevel(5); // max quality by default
+    }
+
+    private void OnEnable()
+    {
+        if (slider == null)
+        {
+            Debug.Log("Get slider");
+            slider = gameObject.GetComponent<Slider>();
+        }
+        slider.value = QualitySettings.GetQualityLevel();
+        
+    }
+
+    private void OnDisable()
+    {
+
+    }
+
+    public void SetQualityLevelFromSlider()
+    {
+        QualitySettings.SetQualityLevel((int)slider.value, true);
+        Debug.Log("quality level = " + QualitySettings.GetQualityLevel());
+        Debug.Log("soft particles = " + QualitySettings.softParticles);
+    }
+}
