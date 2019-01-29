b5b0114721df835e8229657b5063ffdc4a82a215
xacalite <anachronicdesigns@gmail.com>
2019-01-29 07:43:06 -0800

    added null checks to set actvie call on congrats panel (was causing exception sometimes when stopping play)

--- Assets/Scripts/CongratsPanel.cs
+++ Assets/Scripts/CongratsPanel.cs
@@ -22,8 +22,8 @@ public class CongratsPanel : CharmsPanel
     new void OnDisable()
     {
         DisableRigModels();
-        currencyIndicator.SetActive(true);
-        standIcons.SetActive(true);
+        if (currencyIndicator != null) { currencyIndicator.SetActive(true); }
+        if (standIcons != null) { standIcons.SetActive(true); }
         base.OnDisable();
     }
 
