f69daafde2492d3cd4683e530d3efd547613561c
xacalite <anachronicdesigns@gmail.com>
2019-02-06 15:58:01 -0800

    changing implementation in ad button script to use Advertisement namespace instead of Monetization; removing the Unity Monetization SDK (that was from asset store and was causing the dex Android build conflict); now relying on package manager Ads package through Unity Services

---
+++ Assets/Scripts/UnityRewardedAdsButton.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Advertisements;
+
+[RequireComponent(typeof(Button))]
+public class UnityRewardedAdsButton : MonoBehaviour
+{
+
+    public string placementId = "rewardedVideo";
+    private Button adButton;
+
+#if UNITY_IOS
+   private string gameId = "1234567";
+#elif UNITY_ANDROID
+    private string gameId = "7654321";
+#endif
+
+    void Start()
+    {
+        adButton = GetComponent<Button>();
+        if (adButton)
+        {
+            adButton.onClick.AddListener(ShowRewardedAd);
+        }
+
+        if (Advertisement.isSupported)
+        {
+            Advertisement.Initialize(gameId, true);
+        }
+    }
+
+    void Update()
+    {
+        if (adButton)
+        {
+            adButton.interactable = Advertisement.IsReady(placementId);
+        }
+    }
+
+    void ShowRewardedAd()
+    {
+
+        if (Advertisement.IsReady(placementId))
+        {
+            var options = new ShowOptions { resultCallback = HandleShowResult };
+            Advertisement.Show(placementId, options);
+        }
+    }
+
+    void HandleShowResult(ShowResult result)
+    {
+        switch (result)
+        {
+            case ShowResult.Finished:
+                Debug.Log("The ad was successfully shown.");
+                //
+                // YOUR CODE TO REWARD THE GAMER
+                // Give coins etc.
+                break;
+            case ShowResult.Skipped:
+                Debug.Log("The ad was skipped before reaching the end.");
+                break;
+            case ShowResult.Failed:
+                Debug.LogError("The ad failed to be shown.");
+                break;
+        }
+    }
+}
