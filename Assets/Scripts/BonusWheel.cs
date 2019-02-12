3868ba9b5d5bb98ad2e82f2658e25b22835c2fd5
xacalite <anachronicdesigns@gmail.com>
2019-02-12 10:18:22 -0800

    simplifying wheel spinning code; removing all randomization except for prize number; making each spin feel a similar speed/force despite still resulting in a random prize

--- Assets/Scripts/BonusWheel.cs
+++ Assets/Scripts/BonusWheel.cs
@@ -7,7 +7,6 @@ public class BonusWheel : MonoBehaviour
 {
     public BonusPanel bonusPanel;
     public Transform wheelModel;
-    private bool randomStartingPosition = true;
     public Text prizeText;
     public Text spinTimeText;
 
@@ -22,12 +21,7 @@ public class BonusWheel : MonoBehaviour
     void OnEnable()
     {
         spinning = false;
-        anglePerItem = 360 / prize.Count;
-
-        if (randomStartingPosition)
-        {
-            RandomizeStartRotation();
-        }
+        anglePerItem = 360 / prize.Count;       
     }
 
     void Update()
@@ -40,32 +34,13 @@ public class BonusWheel : MonoBehaviour
 #endif
     }
 
-    private void RandomizeStartRotation()
-    {
-        int randomAngle = Random.Range(0, 360);
-        wheelModel.eulerAngles = new Vector3(0, 0, randomAngle);
-        //Debug.Log("Randomize start rotation to " + randomAngle);
-    }
-
     public void Spin()
     {
         if (spinning) { return; }
-
-        float minRandomTimeMultiplier = 1f;
-        float maxRandomTimeMultiplier = 3f;
-        randomTimeMultiplier = Random.Range(minRandomTimeMultiplier, maxRandomTimeMultiplier);
         itemNumber = Random.Range(0, prize.Count);
-        float maxAngle = 360 * maxRandomTimeMultiplier + (itemNumber * anglePerItem);
-
-        float baseTime = 10f;
-        float adjustedTime = baseTime * randomTimeMultiplier;
-
-
-        if (spinTimeText != null)
-        {
-            spinTimeText.text = adjustedTime.ToString();
-        }
-        StartCoroutine(SpinTheWheel(adjustedTime, maxAngle));
+        float maxAngle = 720 + (itemNumber * anglePerItem);
+        float baseTime = 17f;
+        StartCoroutine(SpinTheWheel(baseTime, maxAngle));
     }
 
     IEnumerator SpinTheWheel(float time, float maxAngle)
@@ -77,7 +52,6 @@ public class BonusWheel : MonoBehaviour
         maxAngle = maxAngle - startAngle;
 
         int animationCurveNumber = Random.Range(0, animationCurves.Count);
-        //Debug.Log("Animation Curve No. : " + animationCurveNumber);
 
         while (timer < time)
         {
@@ -91,7 +65,6 @@ public class BonusWheel : MonoBehaviour
         wheelModel.eulerAngles = new Vector3(0.0f, 0.0f, maxAngle + startAngle);
         spinning = false;
 
-        //Debug.Log("Prize: " + prize[itemNumber]);//use prize[itemNumnber] as per requirement
         if (prizeText != null)
         {
             prizeText.text = prize[itemNumber].ToString();
