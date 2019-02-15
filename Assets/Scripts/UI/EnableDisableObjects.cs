98ef7322146db9a953c94d302d122e4b5f299dd9
xacalite <anachronicdesigns@gmail.com>
2019-02-15 10:27:08 -0800

    adding UI controller script for switching between charm sets

---
+++ Assets/Scripts/UI/EnableDisableObjects.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDisableObjects : MonoBehaviour
+{
+    public GameObject[] toEnable;
+    public GameObject[] toDisable;
+
+    public void EnableDisableGameObjects()
+    {
+        if (toEnable == null || toDisable == null)
+        {
+            Debug.LogError("Null array in EnableDisableObjects");
+            return;
+        }
+
+        // enable objects
+        for (int e = 0; e < toEnable.Length; e++)
+        {
+            if (toEnable[e] == null)
+            {
+                Debug.LogError("Cannot enable null object, in EnableDisableObjects");
+                return;
+            }
+
+            toEnable[e].SetActive(true);
+        }
+
+        // disable objects
+        for (int d = 0; d < toDisable.Length; d++)
+        {
+            if (toDisable[d] == null)
+            {
+                Debug.LogError("Cannot disable null object, in EnableDisableObjects");
+                return;
+            }
+
+            toDisable[d].SetActive(false);
+        }
+    }
+}
