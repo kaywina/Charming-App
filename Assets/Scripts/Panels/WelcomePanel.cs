a6473bc7b2359037c709f7b95d27387028be0062
xacalite <anachronicdesigns@gmail.com>
2019-04-23 11:19:11 -0700

    welcome screen now goes to daily bonus spinwheel instead of straight to main ui

---
+++ Assets/Scripts/Panels/WelcomePanel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WelcomePanel : CharmsPanel
+{
+    public GameObject bonusPanel;
+
+    new private void OnEnable()
+    {
+        base.OnEnable();
+    }
+
+    new private void OnDisable()
+    {
+        bonusPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
