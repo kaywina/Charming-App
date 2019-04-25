9673116734f1f88bc5a8986cfccaeab04726a4bf
xacalite <anachronicdesigns@gmail.com>
2019-04-25 13:10:37 -0700

    implementing swipe to switch charms sets for touch on mobile devices (still needs testing); line endings

--- Assets/Scripts/UI/SwipeCharmControls.cs
+++ Assets/Scripts/UI/SwipeCharmControls.cs
@@ -33,50 +33,83 @@ public class SwipeCharmControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        #if UNITY_EDITOR || UNITY_STANDALONE
+#if UNITY_EDITOR || UNITY_STANDALONE
+        SimulateSwipesInEditor();
+#endif
+#if UNITY_IOS || UNITY_ANDROID
+        DetectSwipesOnMobile();
+#endif
+    }
+
+    private void DetectSwipesOnMobile()
+    {
+        if (Input.touchCount == 1)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Began)
+            {
+                startPosition = touch0.position;
+            }
+
+            else if (touch0.phase == TouchPhase.Ended)
+            {
+                endPosition = touch0.position;
+                CheckForSwipe(startPosition, endPosition);
+            }
+        }
+    }
+
+    private void SimulateSwipesInEditor()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             startPosition = Input.mousePosition;
         }
-        else if (Input.GetMouseButtonUp(0)) 
+        else if (Input.GetMouseButtonUp(0))
         {
             endPosition = Input.mousePosition;
+            CheckForSwipe(startPosition, endPosition);
+        }
+    }
 
-            // calculate if mouse movement was a swipe
-            //Debug.Log("swipe from " + startPosition + " to " + endPosition);
-            float xDifference = startPosition.x - endPosition.x;
-            float yDifference = startPosition.y - endPosition.y;
+    private void CheckForSwipe(Vector3 startPos, Vector3 endPos)
+    {
+        // calculate if mouse movement was a swipe
+        //Debug.Log("swipe from " + startPosition + " to " + endPosition);
+        float xDifference = startPos.x - endPos.x;
+        float yDifference = startPos.y - endPos.y;
 
 
-            if (Mathf.Abs(yDifference) > maxVertical)
+        if (Mathf.Abs(yDifference) > maxVertical)
+        {
+            // Debug.Log("no swipe, too much vertical movement");
+            // do nothing
+        }
+        else if (xDifference > minHorizontal)
+        {
+            // Debug.Log("swipe from right to left");
+            // go to previous charm set if possible otherwise do nothing
+            if (charmSet == 0)
             {
-                // Debug.Log("no swipe, too much vertical movement");
-                // do nothing
-            }
-            else if (xDifference > minHorizontal)
-            {
-                // Debug.Log("swipe from right to left");
-                // go to previous charm set if possible otherwise do nothing
-                if (charmSet == 0)
-                {
-                    DeactivateObjects(charmSet);
-                    charmSet = 1;
-                    ActivateObjects(charmSet);
-                }
-            }
-            else if (xDifference < -minHorizontal)
-            {
-                // Debug.Log("swipe from left to right");
-                // go to next charm set if possible otherwise do nothing
-                if (charmSet == 1)
-                {
-                    DeactivateObjects(charmSet);
-                    charmSet = 0;
-                    ActivateObjects(charmSet);
-                }
+                DeactivateObjects(charmSet);
+                charmSet = 1;
+                ActivateObjects(charmSet);
             }
         }
-        #endif
+        else if (xDifference < -minHorizontal)
+        {
+            // Debug.Log("swipe from left to right");
+            // go to next charm set if possible otherwise do nothing
+            if (charmSet == 1)
+            {
+                DeactivateObjects(charmSet);
+                charmSet = 0;
+                ActivateObjects(charmSet);
+            }
+        }
     }
 
     private void DeactivateObjects(int index)
