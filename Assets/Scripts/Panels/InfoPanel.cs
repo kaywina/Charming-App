6d1cba6c5e2647b0d6d5ae04d67614d02b2d94ac
xacalite <anachronicdesigns@gmail.com>
2019-06-03 13:28:37 -0700

    hooking up 'show on load' functionality for info panel

---
+++ Assets/Scripts/Panels/InfoPanel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPanel : CharmsPanel
+{
+    new void OnEnable()
+    {
+        if (PlayerPrefs.GetString("ShowInfo") == "false")
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+        base.OnEnable();
+    }
+}
