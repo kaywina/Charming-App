b5b09f50e3cd5ff20953240def0610b9cfbe828c
xacalite <anachronicdesigns@gmail.com>
2019-06-03 13:23:59 -0700

    ui element for 'show on load' toggle on info panel with skeleton code

---
+++ Assets/Scripts/UI/SetPlayerPrefFromToggle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetPlayerPrefFromToggle : MonoBehaviour
+{
+    public string playerPrefName;
+    public Toggle toggle;
+
+    private void OnEnable()
+    {
+        string prefString = PlayerPrefs.GetString(playerPrefName);
+        if (prefString == "false")
+        {
+            toggle.isOn = false;
+        }
+        else
+        {
+            toggle.isOn = true;
+        }
+    }
+
+    public void TogglePlayerPref()
+    {
+    string prefString = PlayerPrefs.GetString(playerPrefName);
+        if (prefString == "false")
+        {
+            PlayerPrefs.SetString(playerPrefName, "true");
+            toggle.isOn = true;
+        }
+        else
+        {
+            PlayerPrefs.SetString(playerPrefName, "false");
+            toggle.isOn = false;
+        }
+    }
+}
