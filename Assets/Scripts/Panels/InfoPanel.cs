171bce753462025ca92b3ccfa1f4a04c611f890d
xacalite <anachronicdesigns@gmail.com>
2019-06-06 11:44:00 -0700

    fix bug with info panel not opening from ? button when show info toggle not set

--- Assets/Scripts/Panels/InfoPanel.cs
+++ Assets/Scripts/Panels/InfoPanel.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class InfoPanel : CharmsPanel
 {
+    bool respectPlayerPref = true; // this is used for the ? button in options
+
     new void OnEnable()
     {
-        if (PlayerPrefs.GetString("ShowInfo") == "false")
+        if (PlayerPrefs.GetString("ShowInfo") == "false" && respectPlayerPref)
         {
             gameObject.SetActive(false);
+            respectPlayerPref = true; // set it back to true after disrespecting playerpref once
             return;
         }
         base.OnEnable();
+    }
+
+    public void DoNotRespectPlayerPrefThisTime ()
+    {
+        respectPlayerPref = false;
     }
 }
