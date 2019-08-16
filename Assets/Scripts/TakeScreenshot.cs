0d08cf64fb88879de6e1814cab6648525c4f2ea8
xacalite <anachronicdesigns@gmail.com>
2019-08-16 15:13:01 -0700

    add an invisible button to GooglePlay_FeatureGraphic object in Graphics scene to take a screenshot of that object; adding image for Google Play Feature, also adding file for logo with no stroke

---
+++ Assets/Scripts/TakeScreenshot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour
+{
+
+    private int shotCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCount = 0;
+    }
+
+    public void TakeShot()
+    {
+        Debug.Log("Take screenshot");
+
+        // take screenshot and increment count so filenames don't overwrite
+        ScreenCapture.CaptureScreenshot("screenshot" + shotCount + ".png", 1);
+        shotCount++;
+
+    }
+}
