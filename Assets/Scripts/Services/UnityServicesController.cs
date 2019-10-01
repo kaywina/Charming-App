e1cfa3213a18ccc09fd21cd0dd425db65321b0a9
xacalite <anachronicdesigns@gmail.com>
2019-10-01 07:53:11 -0700

    moving Unity Service controllers to an additive scene load asynch; this resolves the startup hitch going from info panel to bonus wheel

---
+++ Assets/Scripts/Services/UnityServicesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnityServicesController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadSceneAsync("Unity Services", LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/UI/IAPButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IAPButton : MonoBehaviour
+{
+    private UnityIAPController iapController;
+    public string purchaseID;
+
+    void OnEnable()
+    {
+        GameObject controllerObject = GameObject.FindGameObjectWithTag("IAPController");
+        iapController = controllerObject.GetComponent<UnityIAPController>();
+    }
+
+    public void MakePurchase()
+    {
+        if (iapController == null)
+        {
+            Debug.LogWarning("IAP Controller is null; aborting purchase");
+            return;
+        }
+
+        switch (purchaseID)
+        {
+            case "16":
+                iapController.BuyConsumable16();
+                break;
+            case "32":
+                iapController.BuyConsumable32();
+                break;
+            case "64":
+                iapController.BuyConsumable64();
+                break;
+            case "128":
+                iapController.BuyConsumable128();
+                break;
+            case "256":
+                iapController.BuyConsumable256();
+                break;
+            default:
+                Debug.LogWarning("ProductID not implemented in IAPButton class");
+                break;
+        }
+
+        return;
+    }
+
+}
