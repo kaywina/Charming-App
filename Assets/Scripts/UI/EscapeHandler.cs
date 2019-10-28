432829cdfec3a38c0b8c68d09d0c499d230719cf
xacalite <anachronicdesigns@gmail.com>
2019-10-28 11:46:57 -0700

    adding back button support for Native Android (mapped to KeyCode.Escape)

---
+++ Assets/Scripts/UI/EscapeHandler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * This class handles the case when the escape key is pressed (native back button on Android)
+ * */
+
+public class EscapeHandler : MonoBehaviour
+{
+
+    public Button optionsButton;
+    public Button optionsBackButton;
+    public Button meditateBackButton;
+    public Button bonusSkipButton;
+    public Button bonusOKButton;
+    public Button storeCloseButton;
+    public Button infoOKButton;
+    public Button welcomeOKButton;
+    public Button unlockNoButton;
+    public Button congratsOKButton;
+
+    private Button[] allButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allButtons = new Button[] {
+            optionsButton,
+            optionsBackButton,
+            meditateBackButton,
+            bonusSkipButton,
+            bonusOKButton,
+            storeCloseButton,
+            infoOKButton,
+            welcomeOKButton,
+            unlockNoButton,
+            congratsOKButton
+        };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            for (int i = 0; i < allButtons.Length; i++)
+            {
+                if (allButtons[i].gameObject.activeInHierarchy == true)
+                {
+                    allButtons[i].onClick.Invoke();
+                    return;
+                }
+            }
+        }
+    }
+}
