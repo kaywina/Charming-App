855519833cd0248f85294725bba53d430f6932b3
xacalite <anachronicdesigns@gmail.com>
2019-11-13 10:03:29 -0800

    adding a finger point sprite to main ui charm that shows and is animated until the user taps on charm (does not show in later sessions if tap is made)

---
+++ Assets/Scripts/ActiveUntilDeactivated.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveUntilDeactivated : MonoBehaviour
+{
+
+    public string PlayerPrefName = "UniqueStringValue"; // replace with a unique player pref name in inspector
+
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt(PlayerPrefName) != 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void Deactivate()
+    {
+        PlayerPrefs.SetInt(PlayerPrefName, 1);
+        gameObject.SetActive(false);
+    }
+}
