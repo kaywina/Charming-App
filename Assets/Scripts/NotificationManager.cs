04166d11473dac2a503517307ff7a92a265fbebc
xacalite <anachronicdesigns@gmail.com>
2020-02-18 12:21:26 -0800

    attempting to send a notification 10 seconds after app load; increasing Android API version to 4.4 (19) and iOS to 10.0

---
+++ Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Notifications;
+using Unity.Notifications.Android;
+
+public class NotificationManager : MonoBehaviour
+{
+    private const string CHANNEL_ID = "CharmingAppNotifications";
+
+    private void Start()
+    {
+        CreateAndroidChannel();
+        SendAndroidNotification();
+    }
+
+    private void CreateAndroidChannel()
+    {
+        Debug.Log("Create Android Channel");
+        var channel = new AndroidNotificationChannel()
+        {
+            Id = CHANNEL_ID,
+            Name = "Charming App Notifications Channel",
+            Importance = Importance.High,
+            Description = "Mindfullness notification from Charming App",
+        };
+        AndroidNotificationCenter.RegisterNotificationChannel(channel);
+    }
+
+    private void SendAndroidNotification()
+    {
+        Debug.Log("Send Android notification in 10 seconds");
+        var notification = new AndroidNotification();
+        notification.Title = "Title of notification!";
+        notification.Text = "Test of the notification.";
+        notification.FireTime = System.DateTime.Now.AddSeconds(10);
+
+        AndroidNotificationCenter.SendNotification(notification, CHANNEL_ID);
+    }
+}
