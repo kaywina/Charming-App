1b8009aa69dd9fa56e807adc30dcbecc8477428e
xacalite <anachronicdesigns@gmail.com>
2020-02-18 13:18:58 -0800

    similar basic implementation of an ios notification

--- Assets/Scripts/NotificationManager.cs
+++ Assets/Scripts/NotificationManager.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Notifications;
+using System;
+
+#if UNITY_ANDROID
 using Unity.Notifications.Android;
+#elif UNITY_IOS
+using Unity.Notifications.iOS;
+#endif
 
 public class NotificationManager : MonoBehaviour
 {
@@ -10,10 +15,17 @@ public class NotificationManager : MonoBehaviour
 
     private void Start()
     {
+#if UNITY_ANDROID
         CreateAndroidChannel();
         SendAndroidNotification();
+#elif UNITY_IOS
+        // we are registering for notifications on app start (see mobile notifications project settings)
+        SendIosNotification();
+#endif
+
     }
 
+#if UNITY_ANDROID
     private void CreateAndroidChannel()
     {
         Debug.Log("Create Android Channel");
@@ -37,4 +49,38 @@ public class NotificationManager : MonoBehaviour
 
         AndroidNotificationCenter.SendNotification(notification, CHANNEL_ID);
     }
+#endif
+
+#if UNITY_IOS
+    private void SendIosNotification()
+    {
+        int hours = 0;
+        int minutes = 0;
+        int seconds = 10;
+
+        var timeTrigger = new iOSNotificationTimeIntervalTrigger()
+        {
+            TimeInterval = new TimeSpan(hours, minutes, seconds),
+            Repeats = false
+        };
+
+        var notification = new iOSNotification()
+        {
+            // You can optionally specify a custom identifier which can later be 
+            // used to cancel the notification, if you don't set one, a unique 
+            // string will be generated automatically.
+            Identifier = "_notification_01",
+            Title = "Title",
+            Body = "Scheduled at: " + DateTime.Now.ToShortDateString() + " triggered in 10 seconds",
+            Subtitle = "This is a subtitle, something, something important...",
+            ShowInForeground = true,
+            ForegroundPresentationOption = (PresentationOption.Alert | PresentationOption.Sound),
+            CategoryIdentifier = "category_a",
+            ThreadIdentifier = "thread1",
+            Trigger = timeTrigger,
+        };
+
+        iOSNotificationCenter.ScheduleNotification(notification);
+    }
+#endif
 }
