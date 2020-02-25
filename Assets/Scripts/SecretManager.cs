55b2dfc28f1675119a5d23c3cf31e4b9f7a0a8ae
xacalite <anachronicdesigns@gmail.com>
2020-02-25 11:37:20 -0800

    mocking up 3 secrets

---
+++ Assets/Scripts/SecretManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretManager : MonoBehaviour
+{
+    public GameObject canvasInteractable;
+    public GameObject canvasNoninteractable;
+    public Secret[] secrets;
+    public GameObject worldSpaceSecrets;
+    public GameObject canvasSecret;
+
+    private int lastIndex = 0;
+
+    public void WhenShowSecret(int i)
+    {
+        canvasInteractable.SetActive(false);
+        canvasNoninteractable.SetActive(false);
+        worldSpaceSecrets.SetActive(true);
+        secrets[i].Show();
+        canvasSecret.SetActive(true);
+        lastIndex = i;
+    }
+
+    public void WhenHideSecret()
+    {
+        canvasSecret.SetActive(false);
+        canvasInteractable.SetActive(true);
+        canvasNoninteractable.SetActive(true);
+        worldSpaceSecrets.SetActive(false);
+        secrets[lastIndex].Hide();
+    }
+}
