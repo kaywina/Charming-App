1cb0cdfd7d43ad7a4386c7b78d7acde60b7d04b6
xacalite <anachronicdesigns@gmail.com>
2020-02-27 11:49:18 -0800

    adding skeleton script for IronSourceManager; adding app keys from dev portal

---
+++ Assets/Scripts/IronSourceManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronSourceManager : MonoBehaviour
+{
+#if UNITY_IOS
+    private const string YOUR_APP_KEY = "b5bf7fe5";
+#elif UNITY_ANDROID
+    private const string YOUR_APP_KEY = "b5bfb96d";
+#else
+    private const string YOUR_APP_KEY = "";
+#endif
+
+    private void Start()
+    {
+        IronSource.Agent.init(YOUR_APP_KEY); // this way auto-initializes the ad units specified in the IS dev portal
+    }
+
+    void OnApplicationPause(bool isPaused)
+    {
+        IronSource.Agent.onApplicationPause(isPaused);
+    }
+
+    public void Validate()
+    {
+        IronSource.Agent.validateIntegration();
+    }
+}
