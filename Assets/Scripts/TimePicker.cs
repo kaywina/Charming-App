89677b17dc7939b9f58dd7bb3745c4e583d59834
xacalite <anachronicdesigns@gmail.com>
2020-03-02 10:18:32 -0800

    skeleton ui framework for picking hour out of 23 hour day

---
+++ Assets/Scripts/TimePicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimePicker : MonoBehaviour
+{
+
+    public Text hourText;
+
+    private int hour = 11; // 11:00 is default time
+    private const int MAX_HOUR = 23; // set for a 24-hour day cycle
+
+    public void HourUp()
+    {
+        hour++;
+
+        // cycle back to zero if too high
+        if (hour > MAX_HOUR)
+        {
+            hour = 0;
+        }
+
+        SetHourText();
+        Debug.Log("Hour up");
+    }
+
+    public void HourDown()
+    {
+        hour--;
+
+        // cycle back to 23 if too low
+        if (hour < 0)
+        {
+            hour = MAX_HOUR;
+        }
+
+        SetHourText();
+        Debug.Log("Hour down");
+    }
+
+    private void SetHourText()
+    {
+        hourText.text = hour.ToString();
+    }
+}
