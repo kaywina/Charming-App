afafc0546678cde9691f5daa8a77f68b777d1223
xacalite <anachronicdesigns@gmail.com>
2020-03-16 10:03:06 -0700

    show a gold lock image instead of particle effect buttons if "Gold" playerpref is not "true"; i.e. only show all particle effect buttons if gold subscription playerpref has been enabled via temporary toggle. Buttons do not show/hide until you exit options and return to that panel

---
+++ Assets/Scripts/EnableFromPlayerPrefToggle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableFromPlayerPrefToggle : MonoBehaviour
+{
+    public SetPlayerPrefFromToggle togglePrefab;
+    public GameObject[] enableIfFalse;
+    public GameObject[] enableIfTrue;
+
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Debug.Log("Enable or disable objects for playerpref " + togglePrefab.GetPlayerPrefName());
+
+        string playerPref = PlayerPrefs.GetString(togglePrefab.GetPlayerPrefName());
+        Debug.Log("playerPref is " + playerPref);
+
+        if (!PlayerPrefs.HasKey(togglePrefab.GetPlayerPrefName()) || playerPref == "false")
+        {
+            EnableDisableObjects(false);
+            Debug.Log("No key or playerpref is false for " + togglePrefab.GetPlayerPrefName());
+        }
+        else
+        {
+            EnableDisableObjects(true);
+            Debug.Log("Key exists and playerpref is true" + togglePrefab.GetPlayerPrefName());
+        }
+    }
+
+    private void EnableDisableObjects(bool isTrue)
+    {
+        for (int i = 0; i < enableIfFalse.Length; i++)
+        {
+            enableIfFalse[i].SetActive(!isTrue);
+        }
+
+        for (int i = 0; i < enableIfTrue.Length; i++)
+        {
+            enableIfTrue[i].SetActive(isTrue);
+        }
+    }
+
+}
