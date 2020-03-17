130210e357e0014ad358b2a2f74a1a95e0b6b013
xacalite <anachronicdesigns@gmail.com>
2020-03-17 09:57:24 -0700

    creating SubscribePanel script and hooking up buttons in scene to handle different cases for opening the subscribe panel from the main ui versus options panel

---
+++ Assets/Scripts/Panels/SubscribePanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubscribePanel : CharmsPanel
+{
+
+    public bool fromOptions = false;
+    public OptionsPanel optionsPanel;
+
+    private bool optionsRTM;
+
+    public void SetFromOptionsFlag (bool flag)
+    {
+        fromOptions = flag;
+    }
+
+    new void OnEnable()
+    {
+        base.OnEnable();
+        if (fromOptions) {
+            returnToMain = false;
+            optionsRTM = optionsPanel.returnToMain;
+            optionsPanel.SetReturnToMain(false);
+            optionsPanel.gameObject.SetActive(false);
+        }
+        else
+        {
+            returnToMain = true;
+        }
+    }
+
+    new void OnDisable()
+    {
+        base.OnDisable();
+        if (fromOptions) {
+            optionsPanel.SetReturnToMain(optionsRTM);
+            optionsPanel.gameObject.SetActive(true);
+        }
+    }
+}
