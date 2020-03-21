ad4d592fe875d635432923cc1f3254ad0c45c0f9
xacalite <anachronicdesigns@gmail.com>
2020-03-21 15:38:12 -0700

    iap button disables the promo text when it receives message purchase was successful

--- Assets/Scripts/UI/IAPButton.cs
+++ Assets/Scripts/UI/IAPButton.cs
@@ -6,29 +6,47 @@ public class IAPButton : MonoBehaviour
 {
     public string purchaseID;
     public GameObject failIndicator;
+    public GameObject promo;
+    public GameObject success;
 
     private void OnEnable()
     {
         EventManager.StartListening(UnityIAPController.failedToSubscribePlayerPref, ShowFailIndicator);
-        EventManager.StartListening(UnityIAPController.subscribeSuccessPlayerPref, HideFailIndicator);
-        HideFailIndicator();
+        EventManager.StartListening(UnityIAPController.subscribeSuccessPlayerPref, OnSuccess);
+
+        failIndicator.SetActive(false);
+
+        if (PlayerPrefs.GetString(UnityIAPController.goldSubscriptionPlayerPref) == "true")
+        {
+            promo.SetActive(false);
+            success.SetActive(true);
+
+        }
+        else
+        {
+            promo.SetActive(true);
+            success.SetActive(false);
+        }
+        
     }
 
     private void OnDisable()
     {
         EventManager.StopListening(UnityIAPController.failedToSubscribePlayerPref, ShowFailIndicator);
-        EventManager.StopListening(UnityIAPController.subscribeSuccessPlayerPref, HideFailIndicator);
+        EventManager.StopListening(UnityIAPController.subscribeSuccessPlayerPref, OnSuccess);
     }
 
     public void MakePurchase()
     {
-        // these have been disabled following change from consumables store to gold subscription
+        // always deactivate fail indicator as part of iap setup
+        failIndicator.SetActive(false);
+
         switch (purchaseID)
         {
             case "Gold":
                 UnityIAPController.BuyGoldSubscription();
                 break;
-            /*
+            /* // these have been disabled following change from consumables store to gold subscription
             case "16":
                 iapController.BuyConsumable16();
                 break;
@@ -55,12 +73,16 @@ public class IAPButton : MonoBehaviour
 
     public void ShowFailIndicator()
     {
+        Debug.Log("IAP Button receives event message that purchase has failed");
         if (failIndicator != null) { failIndicator.SetActive(true); }
     }
 
-    public void HideFailIndicator()
+    public void OnSuccess()
     {
+        Debug.Log("IAP Button receives event message that purchase is successfull");
         if (failIndicator != null) { failIndicator.SetActive(false); }
+        if (promo != null) { promo.SetActive(false); }
+        if (success != null) { success.SetActive(true); }
     }
 
 }
