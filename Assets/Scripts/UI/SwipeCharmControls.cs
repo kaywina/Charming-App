44055481d5757c64398e41d0555832fdab1f4094
xacalite <anachronicdesigns@gmail.com>
2020-03-25 08:00:22 -0700

    refactoring swipe controls so scripts inherit base functionality instead of duplicating code

--- Assets/Scripts/UI/SwipeCharmControls.cs
+++ Assets/Scripts/UI/SwipeCharmControls.cs
@@ -2,122 +2,45 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwipeCharmControls : MonoBehaviour
+public class SwipeCharmControls : SwipeFunction
 {
-
     public GameObject[] charmSets;
     public GameObject[] unlockSets;
     public GameObject[] arrowButtons;
 
-    private Vector3 startPosition;
-    private Vector3 endPosition;
-
-    private float maxVertical = 200f;
-    private float minHorizontal = 30f;
-
-    // do nothing unless swipe is between 7.5% and 62% of the way down the screen
-    private float yLimitTop = 0.075f;
-    private float yLimitBottom = 0.62f;
-
     private int charmSet;
 
-    // Start is called before the first frame update
-    void Start()
+    new void Start()
     {
-        startPosition = Vector3.zero;
-        endPosition = Vector3.zero;
+        base.Start();
         charmSet = 0;
     }
 
-    private void OnEnable()
+    new void OnEnable()
     {
+        base.OnEnable();
         charmSet = Charms.GetCharmSet();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void SwipeLeft()
     {
-#if UNITY_EDITOR || UNITY_STANDALONE
-        SimulateSwipesInEditor();
-#endif
-#if UNITY_IOS || UNITY_ANDROID
-        DetectSwipesOnMobile();
-#endif
-    }
-
-    private void DetectSwipesOnMobile()
-    {
-        if (Input.touchCount == 1)
+        // go to previous charm set if possible otherwise do nothing
+        if (charmSet == 0)
         {
-            // GET TOUCH 0
-            Touch touch0 = Input.GetTouch(0);
-
-            // APPLY ROTATION
-            if (touch0.phase == TouchPhase.Began)
-            {
-                startPosition = touch0.position;
-            }
-
-            else if (touch0.phase == TouchPhase.Ended)
-            {
-                endPosition = touch0.position;
-                CheckForSwipe(startPosition, endPosition);
-            }
+            DeactivateObjects(charmSet);
+            charmSet = 1;
+            ActivateObjects(charmSet);
         }
     }
 
-    private void SimulateSwipesInEditor()
+    protected override void SwipeRight()
     {
-        if (Input.GetMouseButtonDown(0))
+        // go to next charm set if possible otherwise do nothing
+        if (charmSet == 1)
         {
-            startPosition = Input.mousePosition;
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            endPosition = Input.mousePosition;
-            CheckForSwipe(startPosition, endPosition);
-        }
-    }
-
-    private void CheckForSwipe(Vector3 startPos, Vector3 endPos)
-    {
-        // calculate if mouse movement was a swipe
-        //Debug.Log("swipe from " + startPosition + " to " + endPosition);
-        float startRelativePosY = 1 - (startPosition.y / Screen.height);
-
-        // exit if not in swipable area (around charm and colum); i.e. can't swipe over charm buttons or header
-        if (startRelativePosY < yLimitTop || startRelativePosY > yLimitBottom) { return; } 
-
-        // keep going if swipe is valid
-        float xDifference = startPos.x - endPos.x;
-        float yDifference = startPos.y - endPos.y;
-
-        if (Mathf.Abs(yDifference) > maxVertical)
-        {
-            // Debug.Log("no swipe, too much vertical movement");
-            // do nothing
-        }
-        else if (xDifference > minHorizontal)
-        {
-            // Debug.Log("swipe from right to left");
-            // go to previous charm set if possible otherwise do nothing
-            if (charmSet == 0)
-            {
-                DeactivateObjects(charmSet);
-                charmSet = 1;
-                ActivateObjects(charmSet);
-            }
-        }
-        else if (xDifference < -minHorizontal)
-        {
-            // Debug.Log("swipe from left to right");
-            // go to next charm set if possible otherwise do nothing
-            if (charmSet == 1)
-            {
-                DeactivateObjects(charmSet);
-                charmSet = 0;
-                ActivateObjects(charmSet);
-            }
+            DeactivateObjects(charmSet);
+            charmSet = 0;
+            ActivateObjects(charmSet);
         }
     }
 
---
+++ Assets/Scripts/UI/SwipeFunction.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeFunction : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 endPosition;
+
+    private float maxVertical = 200f;
+    private float minHorizontal = 30f;
+
+    // do nothing unless swipe is between 7.5% and 62% of the way down the screen
+    private float yLimitTop = 0.075f;
+    private float yLimitBottom = 0.62f;
+
+    // Start is called before the first frame update
+    protected void Start()
+    {
+        startPosition = Vector3.zero;
+        endPosition = Vector3.zero;
+    }
+
+    protected void OnEnable()
+    {
+
+    }
+
+    protected void OnDisable()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        SimulateSwipesInEditor();
+#endif
+#if UNITY_IOS || UNITY_ANDROID
+        DetectSwipesOnMobile();
+#endif
+    }
+
+    private void DetectSwipesOnMobile()
+    {
+        if (Input.touchCount == 1)
+        {
+            // GET TOUCH 0
+            Touch touch0 = Input.GetTouch(0);
+
+            // APPLY ROTATION
+            if (touch0.phase == TouchPhase.Began)
+            {
+                startPosition = touch0.position;
+            }
+
+            else if (touch0.phase == TouchPhase.Ended)
+            {
+                endPosition = touch0.position;
+                CheckForSwipe(startPosition, endPosition);
+            }
+        }
+    }
+
+    private void SimulateSwipesInEditor()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            startPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            endPosition = Input.mousePosition;
+            CheckForSwipe(startPosition, endPosition);
+        }
+    }
+
+    private void CheckForSwipe(Vector3 startPos, Vector3 endPos)
+    {
+        // calculate if mouse movement was a swipe
+        //Debug.Log("swipe from " + startPosition + " to " + endPosition);
+        float startRelativePosY = 1 - (startPosition.y / Screen.height);
+
+        // exit if not in swipable area (around charm and colum); i.e. can't swipe over charm buttons or header
+        if (startRelativePosY < yLimitTop || startRelativePosY > yLimitBottom) { return; }
+
+        // keep going if swipe is valid
+        float xDifference = startPos.x - endPos.x;
+        float yDifference = startPos.y - endPos.y;
+
+        if (Mathf.Abs(yDifference) > maxVertical)
+        {
+            // Debug.Log("no swipe, too much vertical movement");
+            // do nothing
+        }
+        else if (xDifference > minHorizontal)
+        {
+            SwipeLeft();
+            //Debug.Log("swipe from right to left");
+            
+        }
+        else if (xDifference < -minHorizontal)
+        {
+            SwipeRight();
+            //Debug.Log("swipe from left to right");
+        }
+    }
+
+    protected virtual void SwipeLeft()
+    {
+
+    }
+
+    protected virtual void SwipeRight()
+    {
+
+    }
+}
