295c10efe4d71e8afcc199e09768ae345e64bc41
xacalite <anachronicdesigns@gmail.com>
2020-04-06 15:48:18 -0700

    setting up a slider to control the emission rate of background particle systems; particle systems do not seem to respond equally to changes in the emission rate though (for some there is a major noticeable difference, for others not so much)

---
+++ Assets/Scripts/UI/EmissionRateSlider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EmissionRateSlider : MonoBehaviour
+{
+    public BackgroundParticles bgParticles;
+    public Slider slider;
+
+    private float defaultRate = 50;
+
+    public void SetEmissionRateFromSliderValues()
+    {
+        Debug.Log("Emission of background particles changed");
+
+        if (slider.value <= 0)
+        {
+            bgParticles.SetEmissionRateMultiplier(defaultRate / 2);
+        }
+        else
+        {
+            bgParticles.SetEmissionRateMultiplier(defaultRate * slider.value);
+        }
+    }
+}
