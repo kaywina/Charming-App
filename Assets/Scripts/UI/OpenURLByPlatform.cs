eb33fb3d6e4ecc8886335f383966b08c4fd77fd8
xacalite <anachronicdesigns@gmail.com>
2020-04-18 10:40:05 -0700

    hooking up a 'how to cancel' button that links to the google play and apple support urls for cancelling subscriptions depending on platform, otherwise the Charming App faq

---
+++ Assets/Scripts/UI/OpenURLByPlatform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURLByPlatform : MonoBehaviour
+{
+    public string iosURL = "";
+    public string androidURL = "";
+
+    private string GetURL()
+    {
+#if UNITY_ANDROID
+        return androidURL;
+#elif UNITY_IOS
+        return iOSURL
+#else
+        Debug.Log("Open URL functionality not defined for this platform;
+        return "http://www.charmingapp.com/faq";
+        return null;
+#endif
+    }
+
+    public void OpenURL()
+    {
+        Application.OpenURL(GetURL());
+    }
+}
