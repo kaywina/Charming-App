79bd6b1066267f58b40c3d67146deb01b3cf9b0b
xacalite <anachronicdesigns@gmail.com>
2020-05-02 12:39:24 -0700

    synchronizing rotation between the heart charm on meditate and main ui

---
+++ Assets/Scripts/SynchronizeRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SynchronizeRotation : MonoBehaviour
+{
+    public Transform targetTransform;
+
+    private void OnEnable()
+    {
+        // get rotation from target object
+        transform.rotation = targetTransform.rotation;
+    }
+
+    private void OnDisable()
+    {
+        // assign rotation to target object
+        targetTransform.rotation = transform.rotation;
+    }
+}
