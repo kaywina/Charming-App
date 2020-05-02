90ab19c2c1456d990005ef87ce35a1e186e33f19
xacalite <anachronicdesigns@gmail.com>
2020-05-02 13:21:27 -0700

    visual options moved to sub panel

---
+++ Assets/Scripts/OptionsSubPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsSubPanel : MonoBehaviour
+{
+    public OptionsPanel optionsPanel;
+
+    private bool oldRTM;
+
+    void OnEnable()
+    {
+        oldRTM = optionsPanel.returnToMain;
+        optionsPanel.SetReturnToMain(false);
+        optionsPanel.gameObject.SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        optionsPanel.SetReturnToMain(oldRTM);
+        optionsPanel.gameObject.SetActive(true);
+    }
+}
