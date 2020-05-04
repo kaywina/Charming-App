f60052d354c481c82022766d11894f72a4fe83fa
xacalite <anachronicdesigns@gmail.com>
2020-05-04 14:17:53 -0700

    rank display skeleton code

---
+++ Assets/Scripts/RankManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankManager : MonoBehaviour
+{
+    public GameObject[] rankTextObjects;
+
+    private string rankPlayerPref = "Rank";  // don't change this in production
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRank();
+    }
+
+    private void SetRank()
+    {
+        if (!PlayerPrefs.HasKey(rankPlayerPref))
+        {
+            PlayerPrefs.SetInt(rankPlayerPref, 0);
+            DisableAllRankTextObjects();
+            EnableRankTextObjectByIndex(0);
+            return;
+        }
+
+        SetRankFromPlayerPref(); // set the rank from data if we get this far
+    }
+
+    private void DisableAllRankTextObjects()
+    {
+        for (int i = 0; i < rankTextObjects.Length; i++)
+        {
+            rankTextObjects[i].SetActive(false);
+        }
+    }
+
+    private void EnableRankTextObjectByIndex(int index)
+    {
+        rankTextObjects[index].SetActive(true);
+    }
+
+    private void SetRankFromPlayerPref()
+    {
+        int index = PlayerPrefs.GetInt(rankPlayerPref);
+        if (index >= rankTextObjects.Length)
+        {
+            Debug.LogError("Error: rank index exceeds lenght of rank text objects array");
+            return;
+        }
+
+        DisableAllRankTextObjects();
+        EnableRankTextObjectByIndex(index);
+        Debug.Log("Rank successfully set from data");
+    }
+}
