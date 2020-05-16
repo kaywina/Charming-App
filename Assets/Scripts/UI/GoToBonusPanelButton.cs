50e6ba6558cefa7f4c00e200a97fee1ae778040b
xacalite <anachronicdesigns@gmail.com>
2020-05-16 14:37:21 -0700

    ui toggles to show/hide bonus wheel on start

--- Assets/Scripts/UI/GoToBonusPanelButton.cs
+++ Assets/Scripts/UI/GoToBonusPanelButton.cs
@@ -9,16 +9,32 @@ public class GoToBonusPanelButton : MonoBehaviour
     public GameObject bonusPanel;
     public GameObject infoPanel;
 
+    public SetPlayerPrefFromToggle bonusWheelToggle;
+
     public void TryOpenBonusPanel()
     {
+        // we show the bonus wheel every new day
         bool canOpen = currencyManager.GetCanOpenBonusPanel();
-        if (canOpen)
+
+        // this falg allows the user to choose whether or not have bonus wheel appear on start with a toggle
+        bool shouldOpen = true;
+        if (PlayerPrefs.HasKey(bonusWheelToggle.GetPlayerPrefName())) {
+            if (PlayerPrefs.GetString(bonusWheelToggle.GetPlayerPrefName()) == "false")
+            {
+                shouldOpen = false;
+            }
+        }
+
+        // if we want to should the bonus wheel
+        if (canOpen && shouldOpen)
         {
             CharmsPanel charmsPanel = infoPanel.GetComponent<CharmsPanel>();
             charmsPanel.DeactivateObjects();
             bonusPanel.SetActive(true);
             infoPanel.SetActive(false);
         }
+
+        // otherwise we don't show the bonus wheel
         else
         {
             CharmsPanel charmsPanel = infoPanel.GetComponent<CharmsPanel>();
