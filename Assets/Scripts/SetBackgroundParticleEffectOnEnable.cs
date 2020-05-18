d52008be1dc4ff638c864f82163d8684df4600fb
xacalite <anachronicdesigns@gmail.com>
2020-05-18 07:29:27 -0700

    mock up the tornado particles as a background particle effect rather than having it seperately with the secrets objects

---
+++ Assets/Scripts/SetBackgroundParticleEffectOnEnable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetBackgroundParticleEffectOnEnable : MonoBehaviour
+{
+
+    public BackgroundParticles bgParticles;
+
+    private int tempIndex;
+    public int indexToActivate = 5; // index 5 is set up to refer to tornado particles in the scene
+
+    // Start is called before the first frame update
+    private void OnEnable()
+    {
+        tempIndex = bgParticles.GetIndex();
+        bgParticles.SetIndex(indexToActivate, true);
+    }
+
+    private void OnDisable()
+    {
+        bgParticles.SetIndex(tempIndex, true);
+    }
+}
