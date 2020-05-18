e1ab7b668051180d01693b321bc7855ce75bb568
xacalite <anachronicdesigns@gmail.com>
2020-05-18 07:41:55 -0700

    can now set the custom particle effect as the background effect with a button in secret options

--- Assets/Scripts/SetBackgroundParticleEffectOnEnable.cs
+++ Assets/Scripts/SetBackgroundParticleEffectOnEnable.cs
@@ -9,16 +9,37 @@ public class SetBackgroundParticleEffectOnEnable : MonoBehaviour
 
     private int tempIndex;
     public int indexToActivate = 5; // index 5 is set up to refer to tornado particles in the scene
+    public GameObject checkmark;
 
     // Start is called before the first frame update
     private void OnEnable()
     {
         tempIndex = bgParticles.GetIndex();
         bgParticles.SetIndex(indexToActivate, true);
+
+        if (checkmark != null)
+        {
+            checkmark.SetActive(false);
+        }
     }
 
     private void OnDisable()
     {
         bgParticles.SetIndex(tempIndex, true);
+    }
+
+    public void SetParticleEffectAsBackground()
+    {
+        tempIndex = indexToActivate;
+        if (checkmark != null)
+        {
+            checkmark.SetActive(true);
+            Invoke("DisableCheckmark", 1f); // only show the checkmark for 2 seconds
+        }
+    }
+
+    private void DisableCheckmark()
+    {
+        checkmark.SetActive(false);
     }
 }
