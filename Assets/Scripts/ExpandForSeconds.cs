9e169ef75810a2fb29f8a3fa843a3c7d088d9c21
xacalite <anachronicdesigns@gmail.com>
2020-05-20 15:38:29 -0700

    transform animation for exploding rings along with planet models

---
+++ Assets/Scripts/ExpandForSeconds.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExpandForSeconds : MonoBehaviour
+{
+    public GameObject toExpand;
+
+    public bool x = true;
+    public bool y = true;
+    public bool z = false;
+
+    public float secondsToExpand = 3f;
+
+    public float sizeIncrement = 1.0f;
+
+    private bool isExpanding = false;
+
+    private Vector3 originalLocalScale;
+    
+    void OnEnable()
+    {
+        originalLocalScale = toExpand.transform.localScale;
+    }
+
+    private void OnDisable()
+    {
+        toExpand.transform.localScale = originalLocalScale;
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("Fixed update");
+        if (isExpanding)
+        {
+            Debug.Log("Expand the object!");
+            Vector3 tempScale = new Vector3();
+
+            if (x) { tempScale.x = toExpand.transform.localScale.x + sizeIncrement; }
+            if (y) { tempScale.y = toExpand.transform.localScale.y + sizeIncrement; }
+            if (z) { tempScale.z = toExpand.transform.localScale.z + sizeIncrement; }
+
+            toExpand.transform.localScale = tempScale;
+        }
+    }
+
+    public void Expand()
+    {
+        isExpanding = true;
+        Invoke("StopExpanding", secondsToExpand);
+    }
+
+    private void StopExpanding()
+    {
+        isExpanding = false;
+        toExpand.transform.localScale = originalLocalScale;
+    }
+}
