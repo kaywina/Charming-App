6b387b8b40b11ca203db447c3c8db834105c096e
xacalite <anachronicdesigns@gmail.com>
2020-05-27 08:39:42 -0700

    bit cleaner implementation but same problem; glitchy and position changes when exiting the secret controls

--- Assets/Scripts/UI/SetRotationFromSlider.cs
+++ Assets/Scripts/UI/SetRotationFromSlider.cs
@@ -13,29 +13,28 @@ public class SetRotationFromSlider : MonoBehaviour
 
     public void SetLocalRotation()
     {
-        Quaternion newRot;
+        Vector3 newRot;
         Vector3 oldRot = t.localEulerAngles;
-        //Vector3 oldRot = t.localEulerAngles;
         // using zero values here instead of storing original values (like in SetPositionFromSlider) because of weird glitch bug on x axis
         switch (axis)
         {
             case RotationAxis.X:
-                newRot = Quaternion.Euler(slider.value, oldRot.y, oldRot.z);
+                newRot = new Vector3 (slider.value, oldRot.y, oldRot.z);
                 //Debug.Log("Set X rotation from slider to " + slider.value);
                 break;
             case RotationAxis.Y:
-                newRot = Quaternion.Euler(oldRot.x, slider.value, oldRot.z);
+                newRot = new Vector3 (oldRot.x, slider.value, oldRot.z);
                 //Debug.Log("Set Y rotation from slider");
                 break;
             case RotationAxis.Z:
-                newRot = Quaternion.Euler(oldRot.x, oldRot.y, slider.value);
+                newRot = new Vector3(oldRot.x, oldRot.y, slider.value);
                 //Debug.Log("Set Z rotation from slider");
                 break;
             default:
-                newRot = Quaternion.Euler(0, 0, 0);
+                newRot = new Vector3 (0, 0, 0);
                 Debug.Log("Hit default case in SetRotationFromSlider; should not occur");
                 break;
         }
-        t.localRotation = newRot;
+        t.localRotation = Quaternion.Euler(newRot); ;
     }
 }
