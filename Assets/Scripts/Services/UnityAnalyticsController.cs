8efaf8d50c3ec52490e42c1cd986fd67f119207b
xacalite <anachronicdesigns@gmail.com>
2020-05-31 10:43:08 -0700

    attempt to send an analytics event when user opens the subscribe panel (subscribe screen visit event)

---
+++ Assets/Scripts/Services/UnityAnalyticsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class UnityAnalyticsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (!Analytics.playerOptedOut)
+        {
+            Analytics.initializeOnStartup = true;
+        }
+        else
+        {
+            Analytics.initializeOnStartup = false;
+        }
+    }
+
+    public static void SendVisitSubscribeScreenEvent()
+    {
+        Debug.Log("Send subscribe screen visit analytics events");
+        AnalyticsEvent.ScreenVisit("Subscribe");
+    }
+}
