52b422e70efe6a2afe25ad1c7c654bb72be0554a
xacalite <anachronicdesigns@gmail.com>
2020-06-26 11:15:20 -0700

    button prefab to open a memory game canvas that costs 2 keys to play

---
+++ Assets/Scripts/PlayManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    public GameObject[] games;
+    public GameObject menuCanvas;
+
+    private int gameCost = 2;
+
+    public void PlayGameByIndex(int index)
+    {
+        menuCanvas.SetActive(false);
+        games[index].SetActive(true);
+        CurrencyManager.WithdrawAmount(gameCost);
+    }
+
+    public void QuitGameByIndex(int index)
+    {
+        games[index].SetActive(false);
+        menuCanvas.SetActive(true);
+    }
+}
