117c6fdce11956bf551e698e4e93258406fa93b1
xacalite <anachronicdesigns@gmail.com>
2020-06-26 10:34:35 -0700

    play panel now shows currency indicator

---
+++ Assets/Scripts/Panels/PlayPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPanel : CharmsPanel
+{
+    public GameObject headerControls;
+    public GameObject arrowControls;
+
+    new void OnEnable()
+    {
+        base.OnEnable();
+        headerControls.SetActive(false);
+        arrowControls.SetActive(false);
+        canvasOverlay.SetActive(true); // inherited
+}
+
+    new void OnDisable()
+    {
+        base.OnDisable();
+        headerControls.SetActive(true);
+        arrowControls.SetActive(true);
+        canvasOverlay.SetActive(true); // inherited
+    }
+}
