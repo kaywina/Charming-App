e15b7b256c6f6919bf891d007fd499efcda0ffc7
xacalite <anachronicdesigns@gmail.com>
2020-06-27 09:06:16 -0700

    filil and array of ints and shuffle it to give the randomized indexes for button sprites in attention game

--- Assets/Scripts/Game/GameAttention.cs
+++ Assets/Scripts/Game/GameAttention.cs
@@ -32,6 +32,47 @@ public class GameAttention : MonoBehaviour
     private void SetupLevel()
     {
         Debug.Log("Setup the level for the attention game");
+
+        // fill array up with numbers from 0 to one less than it's length
+        int[] indexesToShuffle = new int[levelButtons[level].Length];
+        for (int i = 0; i < levelButtons[level].Length; i++)
+        {
+            indexesToShuffle[i] = i;
+        }
+
+        Shuffle(indexesToShuffle); // shuffle those numbers
+
+        /*
+        string output = "";
+        foreach (int i in indexesToShuffle)
+        {
+            output += " " + i.ToString();
+        }
+        Debug.Log("contents of indexesToShuffle after shuffling =" + output);
+        */
+
+
+
+        HideAll();
+    }
+
+    //for shuffle number from array
+    void Shuffle(int[] array)
+    {
+        System.Random _random = new System.Random();
+        int p = array.Length;
+        for (int n = p - 1; n > 0; n--)
+        {
+            int r = _random.Next(1, n);
+            int t = array[r];
+            array[r] = array[n];
+            array[n] = t;
+        }
+    }
+
+    private void HideAll()
+    {
+        Debug.Log("Hide all");
         for (int i = 0; i < levelButtons[level].Length; i++)
         {
             //Debug.Log("Set default sprite for object named - " + levelButtons[level][i].name);
