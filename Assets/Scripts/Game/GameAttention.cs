76440d828c65ad42bbde67e6445b0dd9afff7493
xacalite <anachronicdesigns@gmail.com>
2020-06-27 10:02:37 -0700

    press play button on instructions screen that starts 3 2 1 countdown showing random pattern, then clears numbers from buttons

--- Assets/Scripts/Game/GameAttention.cs
+++ Assets/Scripts/Game/GameAttention.cs
@@ -6,26 +6,19 @@ using UnityEngine.UI;
 public class GameAttention : MonoBehaviour
 {
     private int level = 0;
+
+    public Text countdownText;
+    int countdown = 3;
+
     public PlayManager playManager;
 
-    public GameObject[][] levelButtons;
+    public GameObject[] levelButtons;
+    private Image[] images;
     
-    public GameObject[] buttonsLevel1;
-    public GameObject[] buttonsLevel2;
-    public GameObject[] buttonsLevel3;
-    public GameObject[] buttonsLevel4;
-    public GameObject[] buttonsLevel5;
-    public GameObject[] buttonsLevel6;
-
-    private void Awake()
-    {
-        levelButtons = new GameObject[][] { buttonsLevel1,
-            buttonsLevel2, buttonsLevel2, buttonsLevel3,
-            buttonsLevel4, buttonsLevel5, buttonsLevel6 };
-    }
 
     private void OnEnable()
     {
+        ResetCountdown();
         SetupLevel();
     }
 
@@ -33,9 +26,21 @@ public class GameAttention : MonoBehaviour
     {
         Debug.Log("Setup the level for the attention game");
 
-        // fill array up with numbers from 0 to one less than it's length
-        int[] indexesToShuffle = new int[levelButtons[level].Length];
-        for (int i = 0; i < levelButtons[level].Length; i++)
+
+        // get an array of all button Images for this level
+        images = levelButtons[level].GetComponentsInChildren<Image>();
+
+        if (images.Length <= 0)
+        {
+            Debug.LogError("No button images found; are you missing the inspector hookup? Aborting level setup");
+            return;
+        }
+
+        // fill up an array up with numbers from 0 to one less than it's length
+        int[] indexesToShuffle = new int[images.Length];
+
+        // build the array of indexes that will be used to shuffle buttons
+        for (int i = 0; i < indexesToShuffle.Length; i++)
         {
             indexesToShuffle[i] = i + 1; // we want random indexes to start at 1 so we don't get the first sprite in the array, used when hiding the pattern
         }
@@ -43,7 +48,7 @@ public class GameAttention : MonoBehaviour
         Shuffle(indexesToShuffle); // shuffle those numbers
 
         // check for error
-        if (indexesToShuffle.Length != levelButtons[level].Length)
+        if (indexesToShuffle.Length != images.Length)
         {
             Debug.LogError("Mismatched array lengths in Game Attention; aborting level setup");
             return;
@@ -61,11 +66,13 @@ public class GameAttention : MonoBehaviour
         // this should apply the randomized pattern to the buttons
         for (int f = 0; f < indexesToShuffle.Length; f++)
         {
-            Debug.Log("Set sprite for " + levelButtons[level][f].name + " to " + playManager.GetSpriteByIndex(indexesToShuffle[f]).name);
-            levelButtons[level][f].GetComponent<Image>().sprite = playManager.GetSpriteByIndex(indexesToShuffle[f]);
+            //Debug.Log("Set sprite for " + levelButtons[level][f].name + " to " + playManager.GetSpriteByIndex(indexesToShuffle[f]).name);
+            images[f].sprite = playManager.GetSpriteByIndex(indexesToShuffle[f]);
         }
 
-        //HideAll();
+        levelButtons[level].SetActive(true);
+
+        CountdownToPlay();
     }
 
     //for shuffle number from array
@@ -85,15 +92,18 @@ public class GameAttention : MonoBehaviour
     private void HideAll()
     {
         Debug.Log("Hide all");
-        for (int i = 0; i < levelButtons[level].Length; i++)
+        for (int i = 0; i < images.Length; i++)
         {
             //Debug.Log("Set default sprite for object named - " + levelButtons[level][i].name);
-            levelButtons[level][i].GetComponentInChildren<Image>().sprite = playManager.GetSpriteByIndex(0); // set all sprites to blank
+            images[i].sprite = playManager.GetSpriteByIndex(0); // set all sprites to blank
         }
     }
 
     private void NextLevel()
     {
+
+        levelButtons[level].SetActive(false);
+
         level++;
 
         if (level > levelButtons.Length)
@@ -109,5 +119,35 @@ public class GameAttention : MonoBehaviour
     {
         Debug.LogError("That's the end of the game; thanks for playing");
     }
+
+    public void CountdownToPlay()
+    {
+        Debug.Log("Start 3 2 1 countdown to play");
+        countdownText.text = countdown.ToString();
+        InvokeRepeating("CountdownByOne", 1f, 1f);
+    }
+
+    private void CountdownByOne()
+    {
+        countdown--;
+        if (countdown <= 0)
+        {
+            HideAll();         
+            ResetCountdown();
+        }
+        else
+        {
+            countdownText.text = countdown.ToString();
+        }
+    }
+
+    private void ResetCountdown()
+    {
+        CancelInvoke("CountdownByOne");
+        Debug.Log("Reset countdown");
+        countdown = 3;
+        countdownText.text = "";
+    }
+
 }
 
