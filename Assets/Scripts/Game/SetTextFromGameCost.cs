df914c3e2a07e46bff3ca2b9b158bdcc5079c30a
xacalite <anachronicdesigns@gmail.com>
2020-07-06 15:29:22 -0700

    hide key image and game cost text if user is gold subscriber

--- Assets/Scripts/Game/SetTextFromGameCost.cs
+++ Assets/Scripts/Game/SetTextFromGameCost.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 public class SetTextFromGameCost : MonoBehaviour
 {
     public Text costText;
+    public GameObject imageObject;
 
     void OnEnable()
     {
+        int cost = PlayManager.GetGameCost();
         costText.text = PlayManager.GetGameCost().ToString();
+        if (cost == 0)
+        {
+            imageObject.SetActive(false);
+            costText.gameObject.SetActive(false);
+        }  
     }
 }
