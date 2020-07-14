b1ce783972fea22d717b1c4aff176cd80fc20fb8
xacalite <anachronicdesigns@gmail.com>
2020-07-14 12:37:47 -0700

    ad unit ids are also hard-coded; presumably these need to match the ones in the settings. Using test ad ids for both, loads a dummy ad according to inspector output

--- Assets/Scripts/Services/AdmobController.cs
+++ Assets/Scripts/Services/AdmobController.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using GoogleMobileAds.Api;
+using GoogleMobileAds.Common;
 using UnityEngine;
 
 public class AdmobController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private RewardedAd rewardedAd;  
+
+    public void Start()
     {
-        
+        this.rewardedAd = new RewardedAd(GetAdUnitID());
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the rewarded ad with the request.
+        this.rewardedAd.LoadAd(request);
     }
 
-    // Update is called once per frame
-    void Update()
+    private string GetAdUnitID()
     {
-        
+        string adUnitId;
+#if UNITY_ANDROID
+        adUnitId = "ca-app-pub-3940256099942544/5224354917";
+#elif UNITY_IPHONE
+                adUnitId = "ca-app-pub-3940256099942544/1712485313";
+#else
+                adUnitId = "unexpected_platform";
+#endif
+        return adUnitId;
     }
 }
