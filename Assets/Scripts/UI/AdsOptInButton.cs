d3c656dca83cc16217bc4e0dc4f145e166943a6f
xacalite <anachronicdesigns@gmail.com>
2020-08-06 10:21:38 -0700

    set AllowAds playerpref true or false depending on which button clicked on opt in panel (added the opt out button)

---
+++ Assets/Scripts/UI/AdsOptInButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdsOptInButton : MonoBehaviour
+{
+    public GameObject infoPanel;
+    public GameObject optInPanel;
+    
+    public void OptIn (bool optIn)
+    {
+        if (optIn)
+        {
+            UnityAdsController.AllowAds();
+        }
+        else
+        {
+            UnityAdsController.DisallowAds();
+        }
+
+        infoPanel.SetActive(true);
+        optInPanel.SetActive(false);
+    }
+}
