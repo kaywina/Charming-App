04f4b29744c2750ab38d80cbef374d2140a94b48
xacalite <anachronicdesigns@gmail.com>
2020-08-08 14:57:32 -0700

    fallback script for app initialization on main camera, for case where main ui object(s) left disabled in main scene on build

---
+++ Assets/Scripts/UI/EnableMainUIOnAwake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script is intented as a fall-back for app intiialization, in case where main ui has been left deactivated in scene in a build (possibly as a reuslt of using Inspector Tools -> Disable Main UI, and not re-enabling it before a build)
+ * */
+
+public class EnableMainUIOnAwake : MonoBehaviour
+{
+
+    public GameObject coreObject;
+    public GameObject overlayObject;
+
+    private void Awake()
+    {
+        coreObject.SetActive(true);
+        overlayObject.SetActive(true);
+    }
+}
