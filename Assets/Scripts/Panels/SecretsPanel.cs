cbefaab7d1bfb237005fa43ff782979a2bc8fc51
xacalite <anachronicdesigns@gmail.com>
2020-08-09 15:49:41 -0700

    ui fixes for secret panel; fallback code so that menu is enabled and secrets sub-panel is disabled by default when entering secrets panel from main ui

---
+++ Assets/Scripts/Panels/SecretsPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecretsPanel : CharmsPanel
+{
+    public GameObject secrets;
+    public GameObject menu;
+
+    new void OnEnable()
+    {
+        secrets.SetActive(false);
+        menu.SetActive(true);
+        base.OnEnable();
+    }
+
+    new void OnDisable()
+    {
+        base.OnDisable();
+    }
+}
