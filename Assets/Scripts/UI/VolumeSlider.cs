995ba387644dd66c1ddf877dff9b7fcbd3654e4b
xacalite <anachronicdesigns@gmail.com>
2020-08-10 08:12:24 -0700

    working implementation for both music and sfx sliders with data persistence

--- Assets/Scripts/UI/VolumeSlider.cs
+++ Assets/Scripts/UI/VolumeSlider.cs
@@ -6,18 +6,44 @@ using UnityEngine.UI;
 public class VolumeSlider : MonoBehaviour
 {
 
+    public enum VolumeSliderType { Music, SFX };
+    public VolumeSliderType sliderType;
+
     public Slider slider;
     public SoundManager soundManager;
 
     private void OnEnable()
     {
-        slider.value = soundManager.GetMusicVolumeMultiplier();
+        switch (sliderType) {
+            case VolumeSliderType.Music:
+                slider.value = soundManager.GetMusicVolumeMultiplier();
+                break;
+            case VolumeSliderType.SFX:
+                slider.value = soundManager.GetSoundVolumeMultiplier();
+                break;
+            default:
+                Debug.Log("Not a valid volume slider type");
+                break;
+        }
     }
 
     public void SetVolumeFromSlider()
     {
-        Debug.Log("Set volume from slider");
-        soundManager.SetMusicVolumeMultiplier(slider.value);
+        
+        switch (sliderType)
+        {
+            case VolumeSliderType.Music:
+                Debug.Log("Set music volume from slider");
+                soundManager.SetMusicVolumeMultiplier(slider.value);
+                break;
+            case VolumeSliderType.SFX:
+                Debug.Log("Set sfx volume from slider");
+                soundManager.SetSoundVolumeMultiplier(slider.value);
+                break;
+            default:
+                Debug.Log("Not a valid volume slider type");
+                break;
+        }    
     }
 
     public void ResetVolumeMultiplier()
