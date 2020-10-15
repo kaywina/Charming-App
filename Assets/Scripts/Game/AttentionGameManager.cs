7e6c4aea56e62fc145d75b0bf986dfb284c0e575
xacalite <anachronicdesigns@gmail.com>
2020-10-15 10:37:24 -0700

    fixing bug with Attention game sometimes opening to game controls or high score display

--- Assets/Scripts/Game/AttentionGameManager.cs
+++ Assets/Scripts/Game/AttentionGameManager.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class AttentionGameManager : MonoBehaviour
 {
     public GameObject instructions;
+    public GameObject highScoreDisplay;
     public PlayGame attentionPlayGame;
     public AttentionGame attentionGameControls;
 
+    private void OnDisable()
+    {
+        ResetToInstructions();
+    }
+
     public void PlayGame()
     {
-        Debug.Log("Play game Attention");
+        //Debug.Log("Play game Attention");
         HideInstructions();
         attentionPlayGame.Reset();
         attentionGameControls.gameObject.SetActive(true);
@@ -24,5 +30,12 @@ public class AttentionGameManager : MonoBehaviour
     public void HideInstructions()
     {
         instructions.SetActive(false);
+    }
+
+    public void ResetToInstructions()
+    {
+        highScoreDisplay.SetActive(false);
+        attentionGameControls.gameObject.SetActive(false);
+        instructions.SetActive(true);
     }
 }
