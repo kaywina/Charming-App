50cf42710a59918a8a74ebc72b163be0f0855cc2
xacalite <anachronicdesigns@gmail.com>
2020-10-19 10:16:44 -0700

    higher Rememeber game difficulties unlock at each new rank

--- Assets/Scripts/Game/RememberGameSlider.cs
+++ Assets/Scripts/Game/RememberGameSlider.cs
@@ -63,27 +63,55 @@ public class RememberGameSlider : MonoBehaviour
         // there is where we control whether to show the buttons if a particular rank has been achieved, or the lock if not
         int rank = RankManager.GetRank();
 
-        // for unranked players; lock difficulties above 5 buttons
+        // lock out buttons depending on rank of player
         if (rank == 0)
         {
-            int lockoutAboveThisIndex = 2;
-            if (slider.value <= lockoutAboveThisIndex)
-            {
-                Debug.Log("Show and setup the buttons");
-                lockObject.SetActive(false);
-                gameRemember.EnableButtonsByIndex((int)slider.value);
-                gameRemember.SetupButtons(false);
-            }
-            else
-            {
-                Debug.Log("Show the lock");
-                lockObject.SetActive(true);
-                gameRemember.HideAllButtons(); // hide the last shown set of buttons; ugh this is awful
-            }
+            LockButtonsAboveIndex(2);
+        }
+        else if (rank == 1)
+        {
+            LockButtonsAboveIndex(3);
+        }
+        else if(rank == 2)
+        {
+            LockButtonsAboveIndex(4);
+        }
+        else if(rank == 3)
+        {
+            LockButtonsAboveIndex(5);
+        }
+        else if(rank == 4)
+        {
+            LockButtonsAboveIndex(6);
+        }
+        else if (rank == 5)
+        {
+            LockButtonsAboveIndex(7); // this should unlock everything as long as there are only seven difficulty levels
+        }
+        else
+        {
+            LockButtonsAboveIndex(7); // this should unlock everything as long as there are only seven difficulty levels
         }
 
         int scoreMultiplier = (int)slider.value + 1;
         scoreMultiplierText.text = Localization.GetTranslationByKey("SCORE_MULTIPLIER") + " = " + scoreMultiplier.ToString();
+    }
+
+    private void LockButtonsAboveIndex (int index)
+    {
+        if (slider.value <= index)
+        {
+            Debug.Log("Show and setup the buttons");
+            lockObject.SetActive(false);
+            gameRemember.EnableButtonsByIndex((int)slider.value);
+            gameRemember.SetupButtons(false);
+        }
+        else
+        {
+            Debug.Log("Show the lock");
+            lockObject.SetActive(true);
+            gameRemember.HideAllButtons(); // hide the last shown set of buttons; ugh this is awful
+        }
     }
 
     public int GetValue()
