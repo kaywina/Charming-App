0f6ce77019ff2c25b1e8fe669d0fd6f3ee321de4
xacalite <anachronicdesigns@gmail.com>
2020-10-19 09:39:54 -0700

    lockout buttons above difficulty index 2 for unranked (rank 0) players; there is a bug when exiting and returning to app if slider has been left on the lock when app is closed

--- Assets/Scripts/Game/RememberGameSlider.cs
+++ Assets/Scripts/Game/RememberGameSlider.cs
@@ -7,6 +7,7 @@ public class RememberGameSlider : MonoBehaviour
 {
     public Slider slider;
     public RememberGame gameRemember;
+    public GameObject lockObject;
 
     private bool initialized = false;
     private float defaultIndex = 2;
@@ -18,6 +19,7 @@ public class RememberGameSlider : MonoBehaviour
 
     void OnEnable()
     {
+        lockObject.SetActive(false);
         Initialize();
     }
 
@@ -30,7 +32,7 @@ public class RememberGameSlider : MonoBehaviour
 
     public void Initialize()
     {
-        //Debug.Log("Initialize Remember game slider");
+        Debug.Log("Initialize Remember game slider");
         if (!PlayerPrefs.HasKey(playerPrefName))
         {
             slider.value = defaultIndex;
@@ -58,7 +60,30 @@ public class RememberGameSlider : MonoBehaviour
             PlayerPrefs.SetInt(playerPrefName, (int)slider.value);
         }
 
-        if (initialized) { gameRemember.EnableButtonsByIndex((int)slider.value); }
+        if (initialized) {
+
+            // there is where we control whether to show the buttons if a particular rank has been achieved, or the lock if not
+            int rank = RankManager.GetRank();
+
+            // for unranked players; lock difficulties above 5 buttons
+            if (rank == 0)
+            {
+                int lockoutAboveThisIndex = 2;
+                if (slider.value <= lockoutAboveThisIndex)
+                {
+                    Debug.Log("Show and setup the buttons");
+                    lockObject.SetActive(false);
+                    gameRemember.EnableButtonsByIndex((int)slider.value);
+                    gameRemember.SetupButtons(false);
+                }
+                else
+                {
+                    Debug.Log("Show the lock");
+                    lockObject.SetActive(true);
+                    gameRemember.HideAllButtons(); // hide the last shown set of buttons; ugh this is awful
+                }
+            }
+        }
 
         int scoreMultiplier = (int)slider.value + 1;
         scoreMultiplierText.text = Localization.GetTranslationByKey("SCORE_MULTIPLIER") + " = " + scoreMultiplier.ToString();
