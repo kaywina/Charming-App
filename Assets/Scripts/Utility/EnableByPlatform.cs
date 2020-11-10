91e41546f96cd5915edabca7bef16ee2b3c3f4d5
xacalite <anachronicdesigns@gmail.com>
2020-11-10 13:32:39 -0800

    only show the restore purchase button on ios (not android)

---
+++ Assets/Scripts/Utility/EnableByPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableByPlatform : MonoBehaviour
+{
+    public GameObject gameObjectToEnable;
+    public bool ios;
+    public bool android;
+
+    void OnEnable()
+    {
+        gameObjectToEnable.SetActive(false); // false by default
+
+        // enable object depending on platform
+#if UNITY_IOS
+        if (ios) { gameObjectToEnable.SetActive(true); }
+#elif UNITY_ANDROID
+        if (android) { gameObjectToEnable.SetActive(true); }
+#endif
+    }
+}
