fc1089ff6819d5b1fe6c96190423fc9879ec940d
xacalite <anachronicdesigns@gmail.com>
2020-11-19 12:45:48 -0800

    play explode particles after 1 second (to coincide with end of icon coming together animation)

---
+++ Assets/Scripts/UI/PlayParticlesWithDelay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayParticlesWithDelay: MonoBehaviour
+{
+    public ParticleSystem particles;
+    public float delaySeconds = 1f;
+
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Invoke("PlayParticles", delaySeconds);
+    }
+
+    // Update is called once per frame
+    void OnDisable()
+    {
+        StopParticles();
+    }
+
+    private void PlayParticles()
+    {
+        if (particles != null) { particles.Play(); }
+    }
+
+    private void StopParticles()
+    {
+        if (particles != null) { particles.Stop(); }
+    }
+}
+
