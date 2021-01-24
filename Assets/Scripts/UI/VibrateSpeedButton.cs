ac02168679b4f5e0a087c9af39c5be3c450dcb5c
xacalite <anachronicdesigns@gmail.com>
2021-01-24 13:51:36 -0800

    skeleton code to cycle through a speed index, change button sprite and store data in player pref

---
+++ Assets/Scripts/UI/VibrateSpeedButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VibrateSpeedButton : MonoBehaviour
+{
+
+    private string playerPrefName = "VibrateSpeed";
+    public Sprite[] buttonImageFiles;
+    public Image buttonImage;
+
+    private int speedIndex;
+    private int numberOfSpeeds;
+
+    public void CycleSpeed()
+    {
+        speedIndex++;
+        if (speedIndex >= buttonImageFiles.Length)
+        {
+            speedIndex = 0;
+        }
+
+        buttonImage.sprite = buttonImageFiles[speedIndex];
+        PlayerPrefs.SetInt(playerPrefName, speedIndex);
+        Debug.Log("Speed is " + speedIndex);
+    }
+
+    private void OnEnable()
+    {
+        speedIndex = PlayerPrefs.GetInt(playerPrefName);
+        buttonImage.sprite = buttonImageFiles[speedIndex];
+    }
+}
