694bc24a65d23713fbaaccd52665fc0be44c84c1
xacalite <anachronicdesigns@gmail.com>
2021-04-13 13:21:45 -0700

    show a banner ad at top of love panel (if not gold subscriber)

--- Assets/Scripts/Services/GoogleMobileAdsController.cs
+++ Assets/Scripts/Services/GoogleMobileAdsController.cs
@@ -7,6 +7,7 @@ public class GoogleMobileAdsController : MonoBehaviour
 {
 
     static InterstitialAdGameObject interstitialAd;
+    static BannerAdGameObject bannerAd;
     private GameObject[] taggedObjects; // used to control enabling/disabling of UI so it doesn't interfere with ad; this only works because interstitial ad is always shown on top of main ui instead of other panels at this point
 
     public void Start()
@@ -20,6 +21,8 @@ public class GoogleMobileAdsController : MonoBehaviour
         interstitialAd = MobileAds.Instance
             .GetAd<InterstitialAdGameObject>("Interstitial Ad");
 
+        bannerAd = MobileAds.Instance.GetAd<BannerAdGameObject>("Banner Ad");
+
         // Initialize the Mobile Ads SDK.
         MobileAds.Initialize((initStatus) =>
         {
@@ -28,6 +31,31 @@ public class GoogleMobileAdsController : MonoBehaviour
         });
 
         interstitialAd.LoadAd();
+        bannerAd.LoadAd();
+        HideBannerAd(); // or else it shows up on app load
+    }
+
+    public static void ShowBannerAd()
+    {
+        if (UnityIAPController.IsGold())
+        {
+            return; // don't show ads for gold users
+        }
+
+        bannerAd.Show();
+        //Debug.Log("Show banner ad");
+
+    }
+
+    public static void HideBannerAd()
+    {
+        if (UnityIAPController.IsGold())
+        {
+            return; // no ads for gold users
+        }
+
+        bannerAd.Hide();
+        //Debug.Log("Hide banner ad");
     }
 
     public static void ShowInterstitialAd()
---
+++ Assets/Scripts/Services/ShowBannerAdOnEnable.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBannerAdOnEnable : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GoogleMobileAdsController.ShowBannerAd();
+    }
+
+
+    private void OnDisable()
+    {
+        GoogleMobileAdsController.HideBannerAd();
+    }
+}
