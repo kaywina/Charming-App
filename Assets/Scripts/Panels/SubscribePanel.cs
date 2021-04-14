803001cbb7a039a3db0d9966a8546022f86628ab
xacalite <anachronicdesigns@gmail.com>
2021-04-14 10:13:02 -0700

    in secrets, if user is a gold subscriber then show the button to the info screen about subscriber benefits, if not the show a gold lock button that takes them to subscribe panel

--- Assets/Scripts/Panels/SubscribePanel.cs
+++ Assets/Scripts/Panels/SubscribePanel.cs
@@ -11,6 +11,7 @@ public class SubscribePanel : CharmsPanel
     private bool fromAudioOptions = false;
     private bool fromLove = false;
     private bool fromMeditate = false;
+    private bool fromSecrets = false;
 
     public GameObject charmButtons;
     public GameObject meditationWoldSpace;
@@ -21,19 +22,21 @@ public class SubscribePanel : CharmsPanel
     public OptionsPanel optionsPanel;
     public CharmsPanel lovePanel;
     public MeditatePanel meditatePanel;
+    public SecretsPanel secretsPanel;
 
     public GameObject promo;
     public GameObject success;
 
     private bool optionsRTM;
     private bool loveRTM;
+    private bool secretsRTM;
 
     public void SetFromVisualOptionsFlag(bool flag)
     {
         fromVisualOptions = flag;
     }
 
-    public void SetFromAudioOptionsFlag (bool flag)
+    public void SetFromAudioOptionsFlag(bool flag)
     {
         fromAudioOptions = flag;
     }
@@ -43,14 +46,19 @@ public class SubscribePanel : CharmsPanel
         fromOptions = flag;
     }
 
-    public void SetFromLoveFlag (bool flag)
+    public void SetFromLoveFlag(bool flag)
     {
         fromLove = flag;
     }
 
-    public void SetFromMeditateFlag (bool flag)
+    public void SetFromMeditateFlag(bool flag)
     {
         fromMeditate = flag;
+    }
+
+    public void SetFromSecretsFlag(bool flag)
+    {
+        fromSecrets = flag;
     }
 
     new void OnEnable()
@@ -86,6 +94,12 @@ public class SubscribePanel : CharmsPanel
             meditationWoldSpace.SetActive(false);
             meditatePanel.gameObject.SetActive(false);
         }
+        else if (fromSecrets)
+        {
+            secretsRTM = secretsPanel.returnToMain;
+            secretsPanel.SetReturnToMain(false);
+            secretsPanel.gameObject.SetActive(false);
+        }
         else
         {
             returnToMain = true;
@@ -117,6 +131,11 @@ public class SubscribePanel : CharmsPanel
             meditationWoldSpace.SetActive(true);
             meditatePanel.gameObject.SetActive(true);
             meditateOptionsToggle.Toggle();
+        }
+        else if (fromSecrets)
+        {
+            secretsPanel.SetReturnToMain(secretsRTM);
+            secretsPanel.gameObject.SetActive(true);
         }
         else
         {
