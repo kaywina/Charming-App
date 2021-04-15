5246463bf53492ebebcf4ea42fa1bdd4d112749e
xacalite <anachronicdesigns@gmail.com>
2021-04-15 09:58:45 -0700

    disabling banner ad functionality on love panel for fear of ruining social sharing function; moving hearty and starry buttons up a bit for better spacing again

--- Assets/Scripts/Services/GoogleMobileAdsController.cs
+++ Assets/Scripts/Services/GoogleMobileAdsController.cs
@@ -7,7 +7,7 @@ public class GoogleMobileAdsController : MonoBehaviour
 {
 
     static InterstitialAdGameObject interstitialAd;
-    static BannerAdGameObject bannerAd;
+    //static BannerAdGameObject bannerAd;
     private GameObject[] taggedObjects; // used to control enabling/disabling of UI so it doesn't interfere with ad; this only works because interstitial ad is always shown on top of main ui instead of other panels at this point
 
     public void Start()
@@ -21,7 +21,7 @@ public class GoogleMobileAdsController : MonoBehaviour
         interstitialAd = MobileAds.Instance
             .GetAd<InterstitialAdGameObject>("Interstitial Ad");
 
-        bannerAd = MobileAds.Instance.GetAd<BannerAdGameObject>("Banner Ad");
+        //bannerAd = MobileAds.Instance.GetAd<BannerAdGameObject>("Banner Ad");
 
         // Initialize the Mobile Ads SDK.
         MobileAds.Initialize((initStatus) =>
@@ -31,10 +31,11 @@ public class GoogleMobileAdsController : MonoBehaviour
         });
 
         interstitialAd.LoadAd();
-        bannerAd.LoadAd();
-        HideBannerAd(); // or else it shows up on app load
+        //bannerAd.LoadAd();
+        //HideBannerAd(); // or else it shows up on app load
     }
 
+    /*
     public static void ShowBannerAd()
     {
         if (UnityIAPController.IsGold())
@@ -57,6 +58,7 @@ public class GoogleMobileAdsController : MonoBehaviour
         if (bannerAd != null) { bannerAd.Hide(); }
         //Debug.Log("Hide banner ad");
     }
+    */
 
     public static void ShowInterstitialAd()
     {
